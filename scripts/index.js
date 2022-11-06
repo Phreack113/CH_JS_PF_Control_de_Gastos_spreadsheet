70d172ff1af4cc9172e1e455eadfbd896a89eb0e
Sebastian Talamona <sebastiantalamona@gmail.com>
2022-11-06 20:49:06 -0300

    Eventos origen y categoría 1/2

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,16 @@ Idea: CONTROL DE GASTOS PERSONALES
 Voy a armar un sistema de control de gastos personales que acumule todos los gastos
 cargados en una google spreadsheet a traves de su API.
 La idea principal es que se pueda registrar un gasto con la menor cantidad de clicks posibles.
- - 1ra pantalla: Va a ser un teclado para ingresar el monto gastado
- - 2da pantalla: Botones para seleccionar el origen del dinero gastado (efectivo, debito, credito)
- - 3ra pantalla: Botones para seleccionar la categoría del gasto y un detalle opcional.
+- 1ra pantalla: Va a ser un teclado para ingresar el monto gastado
+- 2da pantalla: Botones para seleccionar el origen del dinero gastado (efectivo, debito, credito)
+- 3ra pantalla: Botones para seleccionar la categoría del gasto y un detalle opcional.
 */
+
+//Almaceno en constantes los nodos de cada sección
+const montoSection = document.querySelector('.montoSection');
+const origenSection = document.querySelector('.origenSection');
+const categSection = document.querySelector('.categSection');
+
 
 const disp = document.querySelector('.monto p'); 
 const botns = document.querySelectorAll('.bot');
@@ -20,6 +26,15 @@ class Gasto {
         this.medioName = medioName;
         this.medioType = medioType;
         this.categoria = categoria;
+    }
+
+    setAmount(monto){
+        //Metodo utilizado para cargar el gasto en el objeto
+        this.monto = monto;
+    }
+    setOrigen(name, type){
+        this.medioName = name;
+        this.medioType = type;
     }
 
     send(){
@@ -47,8 +62,13 @@ const botonera = btn => {
         if (disp.innerHTML != 0 && disp.innerHTML.indexOf('.') < 0){
             disp.innerHTML += btn.innerHTML;
         }
-    } else if (btn.classList.contains('ok')){
-        // console.log(disp.innerHTML);
+    } else if (btn.classList.contains('okBot')){
+        //El usuario termina de ingresar el gasto y presiona OK
+        //Luego almaceno el gasto dentro del objeto, oculto la sección del teclado y desoculto la siguiente seción.
+        gasto.setAmount(parseFloat(disp.innerHTML));
+        montoSection.classList.add('off');
+        origenSection.classList.remove('off');
+
     }
 }
 
@@ -82,13 +102,29 @@ const mediosPago = [
     }
 ];
 
-
-const origen = document.querySelector('.origen');
+//Array con categorías disponibles que llegaran desde la spreadsheet a traves de la API
+const categories = [
+{
+    name: 'Salidas',
+    ppto: 20000,
+    gastoAcum: 8234
+},
+{
+    name: 'Supermercado',
+    ppto: 40000,
+    gastoAcum: 12343
+},
+{
+    name: 'Combustible',
+    ppto: 10000,
+    gastoAcum: 5634
+}
+];
 
 const origenPago = mediosPago => {
-    origen.innerHTML = '';
+    // origenSection.innerHTML = '';
     mediosPago.forEach( medio => {
-        origen.append(oriBtn(medio));
+        origenSection.append(oriBtn(medio));
     });
 };
 
@@ -106,7 +142,14 @@ const oriBtn = medio => {
     </div>
     `;
     btnSrc.addEventListener('click', e => {
-        alert('pepe');
+        //Tomo el valor de nombre y tipo de pago seleccionado
+        const oriName = e.target.querySelector('.oriName').innerHTML;
+        const oriType = e.target.querySelector('.oriType').innerHTML;
+        //Lo cargo en el objeto gasto a traves del método setOrigen
+        gasto.setOrigen(oriName, oriType);
+        //Oculto la seccion de origen y desoculto la sección de categoría
+        origenSection.classList.add('off');
+        categSection.classList.remove('off');        
     });
     return btnSrc;
 }
